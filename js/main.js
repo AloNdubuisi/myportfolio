0929c4eb7d352045563ab7846f6902f6806864f0
Alo Ndubuisi <93560628+AloNdubuisi@users.noreply.github.com>
2022-10-28 20:07:58 +0100

    added the navbar for responsiveness

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navigation = document.querySelector('.navigation');
+// CHANGE THE NAV STYLE ON SCROLL
+window.addEventListener('scroll', ()=>{
+    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
+});
 
-menuToggle.onclick = function () {
-    navigation.classList.toggle('active');
+//=========== MAKING THE NAV RESPONSIVE ========== 
+const nav = document.querySelector('.nav-links');
+const openNavBtn = document.querySelector('#nav-toggle-open');
+const closeNavBtn = document.querySelector('#nav-toggle-close');
+
+
+const openNav = () =>{
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
 }
+openNavBtn.addEventListener('click', openNav);
+
+const closeNav = () =>{
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+closeNavBtn.addEventListener('click', closeNav);
+nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav); 
+});
 
 // ========= POINTER EFFECT ==========
 var cursor = document.querySelector(".cursor");
